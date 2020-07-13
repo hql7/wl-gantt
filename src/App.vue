670b6d525010d1377d4854e394372fb01bd31b59
weilan <17796646068@163.com>
2020-07-13 15:38:57 +0800

    feat: app.vue添加节点示例增加非懒加载时的增加方式

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
         use-real-time
         use-check-column
         use-index-column
-        default-expand-all
         end-date="2019-11-02"
         start-date="2009-9-06"
         date-type="monthAndDay"
@@ -33,70 +32,6 @@ export default {
   name: "app",
   data() {
     return {
-      /* data: [
-        {
-          id: "1",
-          pid: "0",
-          name: "旅行",
-          startDate: "2019-09-07",
-          endDate: "2019-09-09",
-          children: [
-            {
-              id: "1-1",
-              pid: "1",
-              name: "云台之间云台之间云台之间云台之间云台之间云台之间云台之间",
-              pre: ["1-1-1"],
-              startDate: "2019-09-01",
-              endDate: "2019-09-09",
-              children: [
-                {
-                  id: "1-1-1",
-                  pid: "1-1",
-                  name: "日落云巅",
-                  pre: ["1-3"],
-                  startDate: "2019-09-01",
-                  endDate: "2019-09-09"
-                }
-              ]
-            },
-            {
-              id: "1-2",
-              pid: "1",
-              pre: ["1-1-1"],
-              name: "天空之镜",
-              startDate: "2019-09-08",
-              endDate: "2019-10-02"
-            },
-            {
-              id: "1-3",
-              name: "蓬莱之岛",
-              pid: "1",
-              startDate: "2019-10-20",
-              endDate: "2019-11-10"
-            },
-            {
-              id: "1-4",
-              pid: "1",
-              name: "西塘之南",
-              startDate: "2019-10-30",
-              endDate: "2019-11-02"
-            },
-            {
-              pid: "1",
-              id: "1-5",
-              name: "凤凰之缘",
-              startDate: "2020-01-01",
-              endDate: "2020-01-10"
-            }
-          ]
-        },
-        {
-          id: "2",
-          name: "租房子",
-          startDate: "2019-09-20",
-          endDate: "2019-12-31"
-        }
-      ] */
       data: [
         {
           id: "1",
@@ -201,13 +136,29 @@ export default {
     // 添加任务
     taskAdd(item) {
       console.log("添加任务：", item);
-      this.$refs["wl-gantt-demo"].loadTreeAdd(item.id, {
-        pid: item.id,
-        id: "###",
-        name: "一轮新月",
-        startDate: "2019-10-11",
-        endDate: "2019-10-19"
-      });
+      // 非懒加载方式直接设置子数据
+      /* this.$set(
+        item,
+        "children",
+        item.children.concat([
+          {
+            pid: item.id,
+            id: "###",
+            name: "一轮新月",
+            startDate: "2019-10-11",
+            endDate: "2019-10-19"
+          }
+        ])
+      ); */
+      this.$refs["wl-gantt-demo"].loadTreeAdd(item.id, [
+        {
+          pid: item.id,
+          id: "###",
+          name: "一轮新月",
+          startDate: "2019-10-11",
+          endDate: "2019-10-19"
+        }
+      ]);
     },
     // 懒加载
     lazyLoad(tree, treeNode, resolve) {
